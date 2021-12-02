016185cf0e772b58960a5dd80dfcda7a2bc19fc2
Shehan <ggsmnandathilaka@gmail.com>
2021-12-02 08:37:12 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostHistoryComponent } from './components/post-history/post-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCardComponent,
+    PostListComponent,
+    PostHistoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/post-history/post-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-history',
+  templateUrl: './post-history.component.html',
+  styleUrls: ['./post-history.component.scss']
+})
+export class PostHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
