817385487aa3bd93b43541b47a84846296869937
Shehan <ggsmnandathilaka@gmail.com>
2021-12-04 10:48:11 +0530

    Unit testing with jest

--- src/app/components/post-history/post-history.component.spec.ts
+++ src/app/components/post-history/post-history.component.spec.ts
@@ -1,25 +1,125 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PostHistoryComponent } from './post-history.component';
+import {PostHistoryComponent} from './post-history.component';
+import {PostService} from "../../services/post.service";
+import {HttpClient} from "@angular/common/http";
+import {HistoryItem} from "../../models/history-item";
+import {BehaviorSubject, Observable, of, Subject} from "rxjs";
+import {createSpyFromClass, provideAutoSpy, Spy} from "jest-auto-spies";
 
 describe('PostHistoryComponent', () => {
   let component: PostHistoryComponent;
   let fixture: ComponentFixture<PostHistoryComponent>;
+  let postServiceSpy: Spy<PostService>;
+  let httpClientSpy: { get: jest.Mock };
+  httpClientSpy = {get: jest.fn()};
+
+  const testHistoryData: HistoryItem[] = [
+    {
+      id: 2,
+      previous: 1,
+      current: 0,
+      postList: [
+        {
+          userId: 1,
+          id: 2,
+          title: "test2",
+          body: "test2"
+        },
+        {
+          userId: 1,
+          id: 1,
+          title: "test1",
+          body: "test1"
+        }
+      ]
+    },
+    {
+      id: 3,
+      previous: 2,
+      current: 1,
+      postList: [
+        {
+          userId: 1,
+          id: 3,
+          title: "test3",
+          body: "test3"
+        },
+        {
+          userId: 1,
+          id: 1,
+          title: "test1",
+          body: "test1"
+        }
+      ]
+    }
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PostHistoryComponent ]
+      declarations: [PostHistoryComponent],
+      providers: [
+        {provide: HttpClient, useValue: httpClientSpy},
+        {
+          provide: PostService,
+          useValue: createSpyFromClass(PostService,
+            {
+              methodsToSpyOn: ["getLatestHistoryItem", "retrievePosts", "setLatestHistoryItem"]
+            })
+        },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PostHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    postServiceSpy = TestBed.inject<any>(PostService);
+    ((<any>postServiceSpy)._postHistorySubject as BehaviorSubject<HistoryItem>) = new BehaviorSubject<HistoryItem>(new HistoryItem(-1, -1, -1, []));
+    (<any>postServiceSpy).getLatestHistoryItem.mockReturnValue(of());
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('History Revoke', () => {
+
+    beforeEach(() => {
+      // set 2 post history items to historyItem list
+      postServiceSpy.historyItems = testHistoryData;
+    });
+
+    it('should revoke history', () => {
+      let historyItem: HistoryItem = {
+        id: 3,
+        previous: 2,
+        current: 1,
+        postList: [
+          {
+            userId: 1,
+            id: 3,
+            title: "test3",
+            body: "test3"
+          },
+          {
+            userId: 1,
+            id: 1,
+            title: "test1",
+            body: "test1"
+          }
+        ]
+      };
+
+      component.onHistoryRevoke(historyItem, 0);
+      expect(postServiceSpy.historyItems.length).toBe(1);
+    });
+
+  });
+
 });
--- src/app/services/post.service.spec.ts
+++ src/app/services/post.service.spec.ts
@@ -1,16 +1,58 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, TestBed} from '@angular/core/testing';
 
-import { PostService } from './post.service';
+import {PostService} from './post.service';
+import {HttpClient} from "@angular/common/http";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {defer} from "rxjs";
+import {Post} from "../models/post";
+import {HistoryItem} from "../models/history-item";
 
 describe('PostService', () => {
-  let service: PostService;
+  let postService: PostService;
+  let httpClientSpy: { get: jest.Mock };
+  httpClientSpy = {get: jest.fn()};
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PostService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        PostService,
+        {provide: HttpClient, useValue: httpClientSpy}
+      ]
+    });
+    postService = TestBed.inject(PostService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(postService).toBeTruthy();
   });
+
+  /*it('should be return posts', fakeAsync(() => {
+
+    const testHistoryItem: HistoryItem = {
+      id: 1,
+      previous: 0,
+      current: 1,
+      postList: [
+        {
+          userId: 1,
+          id: 1,
+          title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "qui est esse",
+          body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        }
+      ]
+    };
+
+    httpClientSpy.get.mockResolvedValue(defer(() => Promise.resolve(testHistoryItem)));
+
+    postService.getLatestHistoryItem().subscribe((historyItems) => {
+      expect(historyItems).toEqual(testHistoryItem);
+    });
+  }));*/
 });
